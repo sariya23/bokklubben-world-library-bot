1e8b2ab55fcfad857cdd8d978122acc0684c1b36
nikita <dayzwaitik123@yandex.ru>
2026-02-18 14:22:04 +0300

    start

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.config.config import create_config
 from src.parse_args import parse_args
+from aiogram import Bot, Dispatcher
 
 import logging
 import asyncio
@@ -16,6 +17,11 @@ async def main():
     config = create_config(args.env_path)
     logging.info("config load successfully")
     logging.info(f"env type: {config.env_type.env_type}")
+    
+    bot = Bot(token=config.bot.bot_token)
+    dp = Dispatcher()
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
